e1acb7dd0033c2da8aca453e8a08151fa5146468
firyal <firyalnabila02@gmail.com>
2022-12-16 06:46:54 +0700

    Tambah halaman detail data

--- lib/view/search.dart
+++ lib/view/search.dart
@@ -1,18 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class SearchPage extends StatefulWidget {
-  const SearchPage({Key? key}) : super(key: key);
+class AboutPage extends StatefulWidget {
+  const AboutPage({Key? key}) : super(key: key);
 
   @override
-  State<SearchPage> createState() => _SearchPageState();
+  State<AboutPage> createState() => _AboutPageState();
 }
 
-class _SearchPageState extends State<SearchPage> {
+class _AboutPageState extends State<AboutPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text(
-        "tes search",
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'ABOUT',
+          style: GoogleFonts.lexend(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            fontStyle: FontStyle.normal,
+            color: Colors.white,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Color(0xff87A8A1),
+        automaticallyImplyLeading: false,
+        elevation: 1,
+      ),
+
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24, vertical: 24),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+          ),
+        ),
       ),
     );
   }
